1c57ab0784317997822b1e812e56e1fe64a549de
Joao Sousa <joaosousa.pt@gmail.com>
2020-05-20 10:19:52 +0100

    NN First Draft Done. Testing needed.

--- include/NN.h
+++ include/NN.h
@@ -1,17 +1,23 @@
 #ifndef NN_H_INCLUDED
 #define NN_H_INCLUDED
 
-#include "ShortestPath.h"
+#include "DWGraph.h"
 
-class NN : public ShortestPath {
+#include <unordered_set>
+
+class NN {
 private:
-    const DWGraph *G;
+    const DWGraph::DWGraph *G;
+    DWGraph::node_t s;
+    std::list<DWGraph::node_t> path;
+    std::unordered_set<node_t> visited;
 
 public:
-    void nearestNeighbour(DWGraph::node_t s);
-    void initialize(const DWGraph *G);
+    NN();
+    void initialize(const DWGraph::DWGraph *G, DWGraph::node_t s);
     void run();
+    node_t findClosest(node_t u);
 
-}
+};
 
 #endif //NN_H_INCLUDED
--- src/NN.cpp
+++ src/NN.cpp
@@ -2,27 +2,49 @@
 
 #include <utility>
 
+typedef DWGraph::Edge Edge;
+typedef DWGraph::weight_t weight_t;
 typedef DWGraph::node_t node_t;
 
-void NN::initialize(const DWGraph *G) {
-    this->G = G;
-    for (int i = 0; i < G->getNodes().size(); i++) {
-        // set NULL (for each vertex)
-    }
+void NN::initialize(const DWGraph::DWGraph *G_, DWGraph::node_t s_) {
+    this->G = G_;
+    this->s = s_;
 
-    // clear Visited
+    this->path.push_back(this->s);
+    this->visited.clear();
 }
 
-void NN::nearestNeighbour(node_t s) {
-    /*
-    set Ans(0) = s;
-    u' = s;
-    for each int i = 1; i < nVertices
-        add u' to the visited (and take from the non-visited?)
-        find minimal dist u'
-        Ans(i) = u';
+void NN::run() {
 
-    Ans(|V|) = s;  <-- back to the start point
-    return Ans
-    */
+    node_t currentNode = this->s;
+
+    for (int i = 0; i < G->getNodes().size(); ++i) {
+
+        // find u' (minimal dist from the set of non visited)
+        node_t u_ = findClosest(currentNode);
+        this->path.push_back(u_);
+        this->visited.insert(u_);
+        currentNode = u_;
+    }
+    // back to the starting node
+    this->path.push_back(this->s);
+}
+
+node_t NN::findClosest(node_t u) {
+
+    node_t minimizer;
+    weight_t weight = DWGraph::INF;
+
+    for (const Edge &e: G->getAdj(u)) {
+        // node cannot be the starting node (s) and must not have yet been visited
+        if (e.v != this->s && (visited.find(e.v) == visited.end()) ) {
+            // if (minimizer == NULL) {minimizer = e.v; weight = e.w;}
+            if (e.w < weight) {
+                minimizer = e.v;
+                weight = e.w;
+            }
+        }
+    }
+
+    return minimizer;
 }
