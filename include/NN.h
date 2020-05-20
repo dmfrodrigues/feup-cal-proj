fea4d91af83fab55b72afa708ce608bb4806d883
JoaoASousa <joaosousa.pt@gmail.com>
2020-05-20 18:00:01 +0100

    Fixed Compile Errors and findClosest() is now private

--- include/NN.h
+++ include/NN.h
@@ -12,13 +12,14 @@ private:
     const DWGraph::DWGraph *G;
     DWGraph::node_t s;
     std::list<DWGraph::node_t> path;
-    std::unordered_set<node_t> visited;
+    std::unordered_set<DWGraph::node_t> visited;
+
+    DWGraph::node_t findClosest(DWGraph::node_t u);
 
 public:
     NN();
     void initialize(const DWGraph::DWGraph *G, DWGraph::node_t s);
     void run();
-    node_t findClosest(node_t u);
     
     std::list<DWGraph::node_t> getTour() const;
 };
--- src/NN.cpp
+++ src/NN.cpp
@@ -18,7 +18,7 @@ void NN::run() {
 
     node_t currentNode = this->s;
 
-    for (int i = 0; i < G->getNodes().size(); ++i) {
+    for (size_t i = 0; i < G->getNodes().size(); ++i) {
 
         // find u' (minimal dist from the set of non visited)
         node_t u_ = findClosest(currentNode);
@@ -32,14 +32,14 @@ void NN::run() {
 
 node_t NN::findClosest(node_t u) {
 
-    node_t minimizer;
+    node_t minimizer = -1;
     weight_t weight = DWGraph::INF;
 
     for (const Edge &e: G->getAdj(u)) {
         // node cannot be the starting node (s) and must not have yet been visited
         if (e.v != this->s && (visited.find(e.v) == visited.end()) ) {
-            if (minimizer == NULL) {minimizer = e.v; weight = e.w;}
-            else if (e.w < weight) {
+            // if (minimizer == NULL) {minimizer = e.v; weight = e.w;}
+            if (e.w < weight) {
                 minimizer = e.v;
                 weight = e.w;
             }
