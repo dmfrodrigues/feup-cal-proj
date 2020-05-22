420c42edbc10a7875a1c1ea2c8cfbf1be1e8c2f5
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-05-22 20:25:28 +0100

    FInished interface (not building tho)

--- include/Ride.h
+++ include/Ride.h
@@ -12,12 +12,53 @@ private:
     class Event{
     public:
         Event();
-        enum event_type:short{ ecu = -1, ecv = 0, ec = 1};
+        enum class event_type:short{ ecu = -1, ecv = 0, ec = 1};
+        friend std::istream& operator>> (std::istream& is, event_type& e){
+            short x; is >> x;
+            switch(x){
+                case -1:
+                    e= event_type::ecu;
+                    break;
+                case 0:
+                    e = event_type::ecv;
+                    break;
+                case 1:
+                    e = event_type::ec;
+                    break;
+                default:
+                    return is;
+            }    
+            return is;
+        }
+        friend std::ostream& operator<< (std::ostream& os, event_type& e){
+            switch(e){
+                case event_type::ecu:
+                    os << -1;
+                    break;
+                case event_type::ecv:
+                   os << 0;
+                    break;
+                case event_type::ec:
+                    os << 1;
+                    break;
+                default:
+                    return os;
+            }  
+            return os;
+        }
+
+        friend std::istream& operator>> (std::istream& is, Event& e){
+            is >> e.t >> e.a >> e.c;
+            return is;
+        }
+        friend std::ostream& operator<< (std::ostream& os, Event& e){
+            os << e.t << e.a << e.c;
+            return os;
+        }
     private:
-        long long int time;
+        long long int t;
         event_type a;
         long long c;
-    
     };
 
     Van v;
@@ -26,6 +67,10 @@ private:
 
 public:
     Ride();
+
+    friend std::istream& operator>> (std::istream& is, Ride& r);
+
+    friend std::ostream& operator<< (std::ostream& os, Ride& r);
 };
 
 
--- src/Ride.cpp
+++ src/Ride.cpp
@@ -1,2 +1,34 @@
 #include "Ride.h"
 
+std::istream& operator>> (std::istream& is, Ride& r){
+        is >> r.v;
+        
+        std::vector<Client> clients;
+        size_t numClients; is >> numClients;
+        for (size_t i = 0 ; i < numClients ; ++i){
+            Client c;
+            is >> c;
+            clients.push_back(c);
+        }
+        r.C = clients;
+
+        std::vector<Ride::Event> events;
+        size_t numEvents; is >> numEvents;
+        for (size_t i = 0 ; i < numEvents ; ++i){
+            Ride::Event e;
+            is >> e;
+            events.push_back(e);
+        }
+        r.e = events;
+
+        return is;
+    }
+
+std::ostream& operator<< (std::ostream& os, Ride& r){
+	os << r.v << std::endl;
+    os << r.C.size() << std::endl;
+    for (size_t i = 0 ; i < r.C.size() ; ++i) os << r.C.at(i) << std::endl;
+    os << r.e.size() << std::endl;
+    for (size_t i = 0 ; i < r.e.size() ; ++i) os << r.e.at(i) << std::endl;
+    return os;
+}
