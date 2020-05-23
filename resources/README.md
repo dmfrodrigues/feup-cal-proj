77ce99135da8189b55d98bc36ea58dbd814eb327
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-05-24 00:24:49 +0100

    Changed generator

--- resources/README.md
+++ resources/README.md
@@ -8,5 +8,17 @@ make clean      # Cleanup files (except submodules and generated client files)
 ## Running
 
 ```
-./generator NUM_CLIENTS
+./generator NUM_CLIENTS START_TIME END_TIME [-t]
+
+NUM_CLIENTS         Number of clients
+START_TIME          Time clients start arriving (in hours)
+END_TIME            Time clients end arriving (in hours)
+-t                  Consider clients only want to be transported from the station to elsewhere
+```
+
+### Examples
+```sh
+./generator   8 8  8.25 -t > it1_01.clients
+./generator  30 8 22    -t > it1_02.clients
+./generator 100 8 22    -t > it1_03.clients
 ```
--- resources/generator.cpp
+++ resources/generator.cpp
@@ -5,22 +5,31 @@
 #include <vector>
 #include <fstream>
 
+using namespace std;
+
 #define HOURS_TO_MICROS ((long long)(60)*60*1000000)
 #define MIN_TO_MICROS ((long long)(60)*1000000)
-#define START_TIME (8*HOURS_TO_MICROS)
-#define END_TIME (8*HOURS_TO_MICROS + 20*MIN_TO_MICROS)
+
+long long large_rand(){
+    long long r1 = rand();
+    long long r2 = rand();
+    return (r1 << 32) | r2;
+}
 
 void generator(int argc, const char *argv[]){
 
-    if (argc != 2) throw std::invalid_argument("invalid number of arguments");
+    if (argc != 4 && argc != 5) throw invalid_argument("invalid number of arguments");
     int n_res = atoi(argv[1]);
+    long long start_time = atof(argv[2])*HOURS_TO_MICROS;
+    long long end_time   = atof(argv[3])*HOURS_TO_MICROS;
+    bool only_trainstation = (argc == 5 && string(argv[4]) == "-t");
 
     srand(time(NULL));
 
     //Read interesting nodes
-    std::vector<point_t> nodes;
-    std::ifstream ifs ("../map/processed/AMP.points");
-    if (!ifs.is_open()) throw std::runtime_error("Could not open interesting nodes file");
+    vector<point_t> nodes;
+    ifstream ifs ("../map/processed/AMP.points");
+    if (!ifs.is_open()) throw runtime_error("Could not open interesting nodes file");
     int ignore_first_line; ifs >> ignore_first_line;
     while(!ifs.eof()){
         point_t n;
@@ -30,39 +39,34 @@ void generator(int argc, const char *argv[]){
     ifs.close();
 
     //List of names
-    std::vector<std::string> names_list = {"Son","Man","Brandon","Fidel","Jc","Fabian","Dannie","Jerald","Hal","Doug","Maynard","Santos","Rigoberto","Franklyn","Howard","Dorian","Edgardo","Rogelio","Everette","Nicolas","Johnie","Jeffery","Carlos","Mickey","Moises","Brooks","Jay","Sean","Antony","Scottie","Felton","Alejandro","Antonia","Rudy","Nathanael","Rayford","Curt","Delmar","William","Denis","Anton","Antone","Osvaldo","Isaac","Freeman","Morton","Manuel","Samual","Sterling","Lewis","Liane","Alane","Rima","Magnolia","Valencia","An","Vicenta","Margene","Gia","Shery","Saran","Madge","Raeann","Buffy","Thalia","Hulda","Yuki","Lita","Venice","Easter","Sonya","Delois","Cleora","Sharri","Genie","Kyung","Petronila","Veta","Shawana","Adelia","Karine","Agnus","Inell","Ashton","Marian","Henriette","Cecelia","Tula","Arlean","Valarie","Talitha","Solange","Ila","Sara","Marline","Alexandria","Gaynelle","Patria","Jaclyn","Shauna","Josue","Yuk","Evette","Awilda","Berry","Linette","Ralph","Nathalie","Antony","Darci","Rhiannon","Rigoberto","Chrystal","Clarence","Clora","Genny","Renea","Maryam","Justine","Nada","Denisse","Matilda","Johnnie","Julee","Harriette","Carolann","Sharell","Bradley","Olinda","Leana","Belva","Jamila","Anna","Liane","Flavia","Deangelo","Loriann","Georgette","Latisha","Elias","Danita","Deborah","Shirlene","Simone","Alissa","Eleonor","Ethan","Elisabeth","Carrie","Debi","Louann","Carli","Collin","Scott","Hubert","Arcelia","Janell","Neal","Ida","Jazmine","Liberty","Geraldine","Greg","Clifford","Edna","Klara","Janae","Aisha","Laraine","Earnestine","Christinia","Ebonie","Gerardo","Genaro","Luis","Marcene","Hoa","Marg","Era","Virgie","Karri","Gilberte","Floria","Eda","Emilio","Jesica","Creola","Isadora","Logan","Moira","Kenda","Angelia","Theodora","Molly","Alpha","Rupert","Tod","Sherley","Chadwick","Na"};
+    vector<string> names_list = {"Son","Man","Brandon","Fidel","Jc","Fabian","Dannie","Jerald","Hal","Doug","Maynard","Santos","Rigoberto","Franklyn","Howard","Dorian","Edgardo","Rogelio","Everette","Nicolas","Johnie","Jeffery","Carlos","Mickey","Moises","Brooks","Jay","Sean","Antony","Scottie","Felton","Alejandro","Antonia","Rudy","Nathanael","Rayford","Curt","Delmar","William","Denis","Anton","Antone","Osvaldo","Isaac","Freeman","Morton","Manuel","Samual","Sterling","Lewis","Liane","Alane","Rima","Magnolia","Valencia","An","Vicenta","Margene","Gia","Shery","Saran","Madge","Raeann","Buffy","Thalia","Hulda","Yuki","Lita","Venice","Easter","Sonya","Delois","Cleora","Sharri","Genie","Kyung","Petronila","Veta","Shawana","Adelia","Karine","Agnus","Inell","Ashton","Marian","Henriette","Cecelia","Tula","Arlean","Valarie","Talitha","Solange","Ila","Sara","Marline","Alexandria","Gaynelle","Patria","Jaclyn","Shauna","Josue","Yuk","Evette","Awilda","Berry","Linette","Ralph","Nathalie","Antony","Darci","Rhiannon","Rigoberto","Chrystal","Clarence","Clora","Genny","Renea","Maryam","Justine","Nada","Denisse","Matilda","Johnnie","Julee","Harriette","Carolann","Sharell","Bradley","Olinda","Leana","Belva","Jamila","Anna","Liane","Flavia","Deangelo","Loriann","Georgette","Latisha","Elias","Danita","Deborah","Shirlene","Simone","Alissa","Eleonor","Ethan","Elisabeth","Carrie","Debi","Louann","Carli","Collin","Scott","Hubert","Arcelia","Janell","Neal","Ida","Jazmine","Liberty","Geraldine","Greg","Clifford","Edna","Klara","Janae","Aisha","Laraine","Earnestine","Christinia","Ebonie","Gerardo","Genaro","Luis","Marcene","Hoa","Marg","Era","Virgie","Karri","Gilberte","Floria","Eda","Emilio","Jesica","Creola","Isadora","Logan","Moira","Kenda","Angelia","Theodora","Molly","Alpha","Rupert","Tod","Sherley","Chadwick","Na"};
 
-    //Write random client
-    std::string output_filename = "clients_" + std::to_string(rand() % 99999 + 999) + ".clients";
-    std::ofstream ofs (output_filename);
-    if (!ofs.is_open()) throw std::runtime_error("Could not create output file");
-
-    std::list<Client> clients;
+    list<Client> clients;
 
     for (int i = 0 ; i < n_res ; ++i){
         Client temp(
             names_list.at(rand() % names_list.size()),
             rand() % 90000000 + 100000000,
             nodes.at(rand() % nodes.size()).getCoord(),
-            START_TIME + rand() % (END_TIME - START_TIME), 
-            rand() % 2
+            start_time + large_rand() % (end_time - start_time), 
+            (only_trainstation ? 0 : rand() % 2)
         );
         clients.push_back(temp);
     }
 
-    ofs << clients.size() << "\n";
+    cout << clients.size() << "\n";
     for(const Client &c: clients)
-        ofs << c << "\n";
+        cout << c << "\n";
 
 }
 
 int main(int argc, char *argv[]){   
     try{
-        if(argc < 2) throw std::invalid_argument("at least one argument must be provided");
+        if(argc < 2) throw invalid_argument("at least one argument must be provided");
         generator(argc, const_cast<const char **>(argv));
     }
-    catch(const std::invalid_argument &e){
-        std::cout << "Caught exception: " << e.what() << "\n";
-        std::cout << "Usage: ./generator INTERESTING_PATH nres\n";
+    catch(const invalid_argument &e){
+        cout << "Caught exception: " << e.what() << "\n";
+        cout << "Usage: ./generator NUMBER_CLIENTS TIME_START TIME_END\n";
     }
 }
