f286b628048cd9a4bbfead13ffe9c0c720f4d080
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-05-23 23:39:02 +0100

    working on HK

--- empirical/src/main.cpp
+++ empirical/src/main.cpp
@@ -5,6 +5,7 @@
 #include "Dijkstra.h"
 #include "ShortestPath.h"
 #include "ShortestPathAll.h"
+#include "HeldKarp.h"
 
 
 #include <iostream>
@@ -12,7 +13,21 @@
 #include <vector>
 #include <chrono>
 
+typedef DWGraph::weight_t weight_t;
+typedef DWGraph::node_t node_t;
+typedef std::vector<weight_t> VI;
+typedef std::vector<VI> VVI;
 typedef std::chrono::high_resolution_clock hrc;
+
+class weight_matrix : public TravellingSalesman::weight_function {
+private:
+    const VVI &M;
+public:
+    weight_matrix(const VVI &M_):M(M_){}
+    DWGraph::weight_t operator()(const std::unordered_set<DWGraph::node_t> &S, const DWGraph::node_t &u, const DWGraph::node_t &v) const{
+        return M.at(u).at(v);
+    }
+};
 
 int main(){
     std::ofstream ofs("../results.csv");
@@ -29,8 +44,8 @@ int main(){
     std::cout << "Running Kosaraju on graphs...\n";
     std::vector<std::pair<int, long long>> kosarajuTimes;
     for (size_t i = 0 ; i < sizes.size() ; ++i) {
+        std::cout << i << std::endl;
         DFS r;
-        std::cout << generators.at(i).getDWGraph()->getNodes().size() << std::endl;
         DUGraph g = DUGraph(*generators.at(i).getDWGraph());
         auto start_time = hrc::now();
         KosarajuV k(&r);
@@ -45,44 +60,69 @@ int main(){
     for (std::pair<int, long long> pair : kosarajuTimes) ofs << pair.first << "," << pair.second << ",\n";
 
 
-    std::cout << "Running A* on graphs...\n";
-    std::vector<std::pair<int, long long>> astarTimes;
-    for (size_t i = 0 ; i < sizes.size() ; ++i) {
-        //ShortestPath *shortestPath = new Astar();
-        //shortestPath->initialize(generators.at(i).getDWGraph(), 1, 6); <- Pick u and v
-        //shortestPath->run();
-        //astarTimes.push_back(std::make_pair(sizes.at(i),shortestPath->getStatistics().execution_time));
-    }
-    std::cout << "Outputing to file\n";
-    std::cout << "A*\n";
-    for (std::pair<int, long long> pair : astarTimes) ofs << pair.first << "," << pair.second << ",\n";
+    // std::cout << "Running A* on graphs...\n";
+    // std::vector<std::pair<int, long long>> astarTimes;
+    // for (size_t i = 0 ; i < sizes.size() ; ++i) {
+    //     ShortestPath *shortestPath = new Astar();
+    //     shortestPath->initialize(generators.at(i).getDWGraph(), 1, 6); <- Pick u and v
+    //     shortestPath->run();
+    //     astarTimes.push_back(std::make_pair(sizes.at(i),shortestPath->getStatistics().execution_time));
+    // }
+    // std::cout << "Outputing to file\n";
+    // std::cout << "A*\n";
+    // for (std::pair<int, long long> pair : astarTimes) ofs << pair.first << "," << pair.second << ",\n";
 
 
-    std::cout << "Running Dijkstra ";
+    std::cout << "Running Dijkstra\n";
     std::vector<std::pair<int, long long>> dijkstraTimes;
     for (size_t i = 0 ; i < sizes.size() ; ++i){
-        ShortestPathAll *shortestPath = new ShortestPathAll::FromOneMany(new Dijkstra(), 8);
-        shortestPath->initialize(generators.at(i).getDWGraph());
+        std::cout << i << std::endl;
+        ShortestPathOneMany *shortestPath = new Dijkstra();
+        shortestPath->initialize(generators.at(i).getDWGraph(), 1);
         shortestPath->run();
         dijkstraTimes.push_back(std::make_pair(sizes.at(i),shortestPath->getStatistics().execution_time));
     }
     std::cout << "Outputing to file\n";
-    std::cout << "Dijkstra\n";
+    ofs << "Dijkstra\n";
     for (std::pair<int, long long> pair : dijkstraTimes) ofs << pair.first << "," << pair.second << ",\n";
 
+    {
+        std::cout << "Running Held-Karp" << std::endl;
+        std::vector<size_t> sizes({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
+        std::vector<std::pair<int, long long>> heldKarpTimes;
+        for (size_t i = 0 ; i < sizes.size() ; ++i){
+            const size_t &sz = sizes[i];
+            std::list<node_t> nodes;{
+                for(node_t u = 0; u < node_t(sz); ++u)
+                    nodes.push_back(u);
+            }
+            const node_t source_node = 0;
+            VVI M(sz, VI(sz, 0));{
+                for(size_t i = 0; i < sz; ++i){
+                    for(size_t j = 0; j < sz; ++j){
+                        if(i == j) continue;
+                        M[i][j] = rand()%10000 + 1;
+                    }
+                }
+            }
+            weight_matrix w(M);
 
-    std::cout << "Running Held-Karp ";
-    std::vector<std::pair<int, long long>> heldKarpTimes;
-    for (size_t i = 0 ; i < sizes.size() ; ++i){
-        ShortestPathAll *shortestPath = new ShortestPathAll::FromOneMany(new Dijkstra(), 8);
-        shortestPath->initialize(generators.at(i).getDWGraph());
-        shortestPath->run();
-        heldKarpTimes.push_back(std::make_pair(sizes.at(i),shortestPath->getStatistics().execution_time));
+            TravellingSalesman *tsp = new HeldKarp();
+            tsp->initialize(&nodes, source_node, &w);
+            try{
+                tsp->run();
+            } catch(const std::exception &e){
+                std::cerr << "Caught exception" << std::endl;
+                throw e;
+            }
+
+            heldKarpTimes.push_back(std::make_pair(sizes[i], tsp->getStatistics().execution_time));
+            delete tsp;
+        }
+        std::cout << "Outputing to file\n";
+        std::cout << "Held-Karp\n";
+        for (std::pair<int, long long> pair : heldKarpTimes) ofs << pair.first << "," << pair.second << ",\n";
     }
-    std::cout << "Outputing to file\n";
-    std::cout << "Held-Karp\n";
-    for (std::pair<int, long long> pair : heldKarpTimes) ofs << pair.first << "," << pair.second << ",\n";
-
 
     ofs.close();
     return 0;
