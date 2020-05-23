a5001aefa79d43b0c73d7f86c58b91549499cc6c
Joao Sousa <joaosousa.pt@gmail.com>
2020-05-23 21:17:00 +0100

    Documentation on Reachability.h and DFS.h

--- algorithms/include/Reachability.h
+++ algorithms/include/Reachability.h
@@ -3,10 +3,33 @@
 
 #include "DUGraph.h"
 
+/**
+ * @brief Reachability Interface
+ * 
+ */
 class Reachability{
 public:
+    /**
+     * @brief Initializes the Directed Unweighted Graph and the starting data members required by the algorithm
+     * 
+     * @param G     Directed Unweighted Graph
+     * @param s     Starting Node
+     */
     virtual void initialize(const DUGraph *G, DUGraph::node_t s) = 0;
+
+    /**
+     * @brief Computes for every Node if it can be reached starting at s (Starting Node data member)
+     * 
+     */
     virtual void run() = 0;
+
+    /**
+     * @brief Retrieves the boolean value correspondent to the reachability of a Node (starting at Node s)
+     * 
+     * @param v         Node to Test
+     * @return true     If it can be reached
+     * @return false    Otherwise
+     */
     virtual bool is_reachable(DUGraph::node_t v) const = 0;
 };
 
