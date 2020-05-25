6a713b6cd96c513836c5da57eff6903e27e80ecc
Joao Sousa <joaosousa.pt@gmail.com>
2020-05-25 03:39:13 +0100

    .

--- algorithms/include/Kosaraju.h
+++ algorithms/include/Kosaraju.h
@@ -4,6 +4,10 @@
 #include "SCC.h"
 #include <bits/stdc++.h> 
 
+/**
+ * @brief Kosaraju algorithm
+ * 
+ */
 class Kosaraju : public SCC{
 private:
     const DUGraph *G;
@@ -11,11 +15,40 @@ private:
     std::stack<DUGraph::node_t> L;
     std::unordered_map<DUGraph::node_t, DUGraph::node_t> SCCs;
 public:
+    /**
+     * @brief Construct without arguments
+     * 
+     */
     Kosaraju();
+
+    /**
+     * @brief Initializes data members that will be used in the algorithm's execution
+     * 
+     * @param G     Directed Unweighted Graph
+     */
     void initialize(const DUGraph *G);
+
+    /**
+     * @brief Depth-first Search for Kosaraju
+     * 
+     * @param u Starting Node
+     */
     void DFS_K(DUGraph::node_t u);
+
     void assign(DUGraph::node_t u, DUGraph::node_t root);
+
+    /**
+     * @brief Execute the algorithm
+     * 
+     */
     void run();
+
+    /**
+     * @brief Retrieves the root of the scc that the parameter Node belongs to
+     * 
+     * @param v                 Node to operate over
+     * @return DUGraph::node_t  Node that is the root of the parameter's scc
+     */
     DUGraph::node_t get_scc(DUGraph::node_t u) const;
 };
 
