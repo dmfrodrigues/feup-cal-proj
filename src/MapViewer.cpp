0a1507a0f0d3ab147269133be33f551f397c974e
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-03-20 23:58:40 +0000

    Minor changes

--- src/MapViewer.cpp
+++ src/MapViewer.cpp
@@ -15,22 +15,22 @@ MapViewer::MapViewer(window_t w_, window_t h_, coord_t min_, coord_t max_):
 void MapViewer::addNode(node_t i, coord_t c){
     float x = float(+(c.getLon() - nw_corner.getLon())*COORDMULT);
     float y = float(-(c.getLat() - nw_corner.getLat())*COORDMULT);
-    GraphViewer::addNode(GraphViewer::Node(i, sf::Vector2f(x, y)));
+    GraphViewer::addNode(i, sf::Vector2f(x, y));
 }
 
 void MapViewer::addNode(node_t i, coord_t c, const sf::Color &color, window_t w){
     window_t x = (window_t)(double)(+(c.getLon() - nw_corner.getLon())*COORDMULT);
     window_t y = (window_t)(double)(-(c.getLat() - nw_corner.getLat())*COORDMULT);
-    GraphViewer::Node &node = GraphViewer::addNode(GraphViewer::Node(i, sf::Vector2f(x, y)));
+    GraphViewer::Node &node = GraphViewer::addNode(i, sf::Vector2f(x, y));
     node.setColor(color);
     node.setSize(w);
 }
 
 void MapViewer::addEdge(edge_t i, node_t u, node_t v, GraphViewer::Edge::EdgeType edge_type, const sf::Color &color, window_t w, bool dashed){
-    const GraphViewer::Node &uNode = getNode(u);
-    const GraphViewer::Node &vNode = getNode(v);
+    GraphViewer::Node &uNode = getNode(u);
+    GraphViewer::Node &vNode = getNode(v);
     
-    GraphViewer::Edge &edge = GraphViewer::addEdge(GraphViewer::Edge((int)i, &uNode, &vNode, edge_type));
+    GraphViewer::Edge &edge = GraphViewer::addEdge((int)i, uNode, vNode, edge_type);
     edge.setColor(color);
     edge.setThickness(w);
     edge.setDashed(dashed);
